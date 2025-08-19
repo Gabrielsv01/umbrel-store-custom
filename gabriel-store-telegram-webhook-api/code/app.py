e3e9035a4ba9642c7d130761f96ef63e4144d480
Gabrielsv01 <gabriel.vieira@gympass.com>
2025-08-19 09:03:40 -0300

    feat(telegram): add new app in store

---
+++ gabriel-store-telegram-webhook-api/code/app.py
@@ -0,0 +1,36 @@
+from telegram.ext import Updater
+import os
+import requests
+from telegram.ext import MessageHandler, Filters
+
+# URL do seu servidor onde o bot está rodando
+WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
+FORWARD_ENDPOINT = os.environ.get("FORWARD_ENDPOINT")
+TOKEN_API = os.environ.get("TELEGRAM_BOT_TOKEN")
+PORT = os.environ.get("PORT", 5125)
+
+def main():
+    updater = Updater(TOKEN_API, use_context=True)
+    dp = updater.dispatcher
+
+    updater.bot.setWebhook(WEBHOOK_URL)
+    
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path="telegram")
+    
+
+    def forward_message(update, context):
+        if FORWARD_ENDPOINT:
+            data = update.to_dict()
+            try:
+                requests.post(FORWARD_ENDPOINT, json=data, timeout=5)
+            except Exception as e:
+                print(f"Erro ao encaminhar mensagem: {e}")
+
+    dp.add_handler(MessageHandler(Filters.all, forward_message))
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
