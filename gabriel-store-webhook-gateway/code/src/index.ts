0e7cd31ae44f59db962dbfdbf7adcc3f8f950184
Gabrielsv01 <gabriel.vieira@gympass.com>
2025-08-19 11:59:54 -0300

    update version

--- gabriel-store-webhook-gateway/code/src/index.ts
+++ gabriel-store-webhook-gateway/code/src/index.ts
@@ -21,7 +21,7 @@ if (process.env.ENABLE_HELMET === 'true') {
          xssFilter: process.env.ENABLE_XSS_FILTER === 'true'
     }));
 }
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
