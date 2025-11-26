18ae939a7aa688b5546a63db1be9b17c8d1a2862
Gabrielsv01 <gabriel.vieira@gympass.com>
2025-11-26 10:34:10 -0300

    feat: add Makefile for Docker management; update environment variables and version to 1.0.13

--- gabriel-store-telegram-webhook-api/code/index.ts
+++ gabriel-store-telegram-webhook-api/code/index.ts
@@ -1,7 +1,6 @@
-
 import express, { Request, Response } from 'express';
 import axios from 'axios';
-import 'dotenv/config'; 
+import 'dotenv/config';
 
 const { PORT, TELEGRAM_BOT_TOKEN, WEBHOOK_URL, FORWARD_ENDPOINT, URI_BASE, DEBUG } = process.env;
 
@@ -13,10 +12,30 @@ const webhookURL = `${WEBHOOK_URL}${URI}`;
 const app = express();
 app.use(express.json());
 
-
-const setupWebhook = async (): Promise<void> => {
+async function getNgrokPublicUrl(): Promise<string | null> {
+    const ngrokWebUrl = process.env.NGROK_WEB_URL;
+    if (!ngrokWebUrl) {
+        console.error('Variável de ambiente NGROK_WEB_URL não definida');
+        return null;
+    }
     try {
-        await axios.get(`${TELEGRAM_API}/setWebhook?url=${webhookURL}&drop_pending_updates=true`);
+        const res = await axios.get(ngrokWebUrl);
+        const html = res.data as string;
+        const match = html.match(/<th>URL<\/th><td>(https:\/\/[^<]+)<\/td>/);
+        if (match && match[1]) {
+            return match[1];
+        }
+        return null;
+    } catch (err) {
+        console.error('Erro ao buscar ngrok web interface:', err);
+        return null;
+    }
+}
+
+
+const setupWebhook = async (url: string): Promise<void> => {
+    try {
+        await axios.get(`${TELEGRAM_API}/setWebhook?url=${url}&drop_pending_updates=true`);
         console.log('Webhook configurado com sucesso!');
     } catch (error: any) {
         console.error('Erro ao configurar webhook:', error);
@@ -48,7 +67,25 @@ app.post(URI, async (req: Request, res: Response) => {
 });
 
 
+
 app.listen(Number(PORT), async () => {
     console.log(`Servidor rodando na porta ${PORT}`);
-    await setupWebhook();
+    let finalWebhookUrl = webhookURL;
+    if (process.env.NGROK_WEB_URL) {
+        const ngrokUrl = await getNgrokPublicUrl();
+        if (ngrokUrl) {
+            finalWebhookUrl = `${ngrokUrl}${URI}`;
+            console.log('Usando URL do ngrok para webhook:', finalWebhookUrl);
+        } else {
+            console.log('Não foi possível obter a URL do ngrok, usando WEBHOOK_URL padrão.');
+        }
+    } else {
+        console.log('NGROK_WEB_URL não definida, usando WEBHOOK_URL padrão.');
+    }
+    try {
+        await setupWebhook(finalWebhookUrl);
+        console.log('Webhook configurado com sucesso!');
+    } catch (error: any) {
+        console.error('Erro ao configurar webhook:', error);
+    }
 });
