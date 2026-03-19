5de35d965e9960db1dd2507d5df4b1b0f332864d
Gabrielsv01 <gabriel.vieira@gympass.com>
2026-03-19 10:17:47 -0300

    feat: add Makefile for Docker image management and update webhook-gateway image version in Docker Compose

--- gabriel-store-webhook-gateway/code/src/filters/telegramFilter.ts
+++ gabriel-store-webhook-gateway/code/src/filters/telegramFilter.ts
@@ -1,8 +1,8 @@
 function telegramFilter(payload: any, config: any, _headers: any) {
-    const message = payload.message;
+    const message = payload?.message || payload?.my_chat_member;
 
-    const isChatAuthorized = config.authorizedChatIds?.length === 0 || config.authorizedChatIds?.includes(message.chat.id.toString());
-    const isUserAuthorized = config.authorizedUsernames?.length === 0 || (message.from?.username && config.authorizedUsernames?.includes(message.from.username));
+    const isChatAuthorized = config.authorizedChatIds?.length === 0 || config.authorizedChatIds?.includes(message?.chat?.id.toString());
+    const isUserAuthorized = config.authorizedUsernames?.length === 0 || (message?.from?.username && config.authorizedUsernames?.includes(message?.from?.username));
     return isChatAuthorized || isUserAuthorized;
 }
 
